6eb86beb9a6888693fb56811fc00c1e0b296ffb1
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-17 15:20:04 +0530

    add header component, update layout to include header, and enhance home page with background and text styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,13 @@
-
 export default function Home() {
   return (
-    <div className="flex items-center justify-center ">
-      
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="flex flex-col items-center gap-5 justify-center text-center ">
+        <h1 className="text-7xl font-extrabold">Brainly</h1>
+        <p className="text-5xl font-bold lg:text-6xl lg:w-[900px] md:text-5xl md:w-[700px]">
+          A second brain to manage your important links and docs
+        </p>
+      </div>
     </div>
   );
 }
