336d4ba6df86c112bb12648c5f3eafe8173c9325
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-26 12:48:30 +0530

    add getTags controller and route; enhance tag management functionality

--- backend/src/controllers/tag.controller.ts
+++ backend/src/controllers/tag.controller.ts
@@ -2,10 +2,7 @@ import { Request, Response } from "express";
 import Tag from "../schemas/tag.schema";
 import { validationResult } from "express-validator";
 
-export const createTag = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const createTag = async (req: Request, res: Response): Promise<void> => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
@@ -18,6 +15,16 @@ export const createTag = async (
     });
     const data = await tag.save();
     res.json({ success: true, data: data });
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send({ success: false, message: "Internal Server error." });
+  }
+};
+
+export const getTags = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const data = await Tag.find();
+    res.json({ success: true, data });
   } catch (err: any) {
     console.error(err.message);
     res.status(500).send({ success: false, message: "Internal Server error." });
--- backend/src/routes/tag.router.ts
+++ backend/src/routes/tag.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { tagValidator } from "../validations/tag.validator";
-import { createTag } from "../controllers/tag.controller";
+import { createTag, getTags } from "../controllers/tag.controller";
 
 const tagRouter = express.Router();
 
 tagRouter.post("/", tagValidator, createTag);
+tagRouter.get("/", tagValidator, getTags);
 
 export default tagRouter;
